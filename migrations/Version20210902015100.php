64ed59b4a667e1bec5cb087d0a598b63d58bf81e
anablaz <67980649+anablaz@users.noreply.github.com>
2021-09-06 01:42:15 +0200

    Set-up Symfony & Registration

---
+++ migrations/Version20210902015100.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210902015100 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE expert (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(171) NOT NULL, last_name VARCHAR(171) NOT NULL, email VARCHAR(171) NOT NULL, institution VARCHAR(171) DEFAULT NULL, bio LONGTEXT DEFAULT NULL, languages VARCHAR(171) DEFAULT NULL, keywords VARCHAR(171) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE expert');
+        $this->addSql('DROP TABLE user');
+    }
+}
