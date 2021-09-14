0866765c33b7062bfa14fbe1bdf74ebdbbcb6693
anablaz <67980649+anablaz@users.noreply.github.com>
2021-09-14 06:45:16 +0200

    Add full registration form in 3 parts

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,9 +5,15 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -16,34 +22,68 @@ class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder
-            ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
-            ])
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 9,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
-                ],
-            ])
-        ;
+        switch ($options['flow_step']) {
+            case 1:
+                $builder
+                    ->add('first_name', TextType::class)
+                    ->add('last_name', TextType::class)
+                    ->add('email', EmailType::class)
+                    ->add('plainPassword', PasswordType::class, [
+                        'attr' => ['autocomplete' => 'new-password'],
+                        'constraints' => [
+                            new NotBlank([
+                                'message' => 'Please enter a password',
+                            ]),
+                            new Length([
+                                'min' => 9,
+                                'minMessage' => 'Your password should be at least {{ limit }} characters',
+                                // max length allowed by Symfony for security reasons
+                                'max' => 4096,
+                            ]),
+                        ],
+                    ]);
+                break;
+            case 2:
+                $builder
+                    ->add('institution', TextType::class)
+                    ->add('phone', TelType::class)
+                    ->add('cv', TextareaType::class)
+                    ->add('photo', FileType::class, [
+                        'constraints' => [
+                            new File([
+                                'maxSize' => '1023k',
+                                'mimeTypes' => [
+                                    'image/jpeg', 'image/png', 'image/webp'
+                                ],
+                                'mimeTypesMessage' => 'Please, upload a valid format (.jpeg, .jpg, .png, .webp)',
+                            ])
+                        ]
+                    ]);
+                break;
+            case 3:
+                $builder
+                    ->add('languages', TextType::class)
+                    ->add('keywords', TextType::class)
+                    ->add('agreePrivacy', CheckboxType::class, [
+                            'mapped' => false,
+                            'constraints' => [
+                                new IsTrue([
+                                    'message' => 'You should agree to our privacy policy.',
+                                ]),
+                            ],
+                        ])
+                    ->add('agreeTerms', CheckboxType::class, [
+                        'mapped' => false,
+                        'constraints' => [
+                            new IsTrue([
+                                'message' => 'You should agree to our terms and conditions.',
+                            ]),
+                        ],
+                    ]);
+                break;
+        }
+
+
     }
 
     public function configureOptions(OptionsResolver $resolver)
