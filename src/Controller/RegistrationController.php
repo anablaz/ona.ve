82b8ccbfbfd5a1b3fd82def92cc088c93fa6ad66
anablaz <67980649+anablaz@users.noreply.github.com>
2021-09-19 02:29:14 +0200

    Make email sender configurable

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -64,7 +64,6 @@ class RegistrationController extends AbstractController
                 // generate a signed url and email it to the user
                 $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                     (new TemplatedEmail())
-                        ->from(new Address('onaVeVerifikacija@gmail.com', 'Ona Ve'))
                         ->to($user->getEmail())
                         ->subject('Please Confirm your Email')
                         ->htmlTemplate('registration/confirmation_email.html.twig')
